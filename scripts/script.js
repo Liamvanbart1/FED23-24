e59d98ced12a5b646ae385f2b69c9ed639ac3af9
Liamvanbart1 <90309539+Liamvanbart1@users.noreply.github.com>
2023-09-26 12:15:44 +0200

    Veranderingen

--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,8 @@
 // JavaScript Document
+var hamburgerMenu = document.querySelector("header > div");
 
-
-//Inlog button//
-var deHeader =
-document.querySelector("header");
-
-var inlogButton = 
-document.querySelector("header > div > button:nth-of-type(1)");
-
-inlogButton.addEventListener("click", inlogToggle);
-
-function inlogToggle() {
-    deHeader.classList.toggle("inlogOpen");
-    deHeader.classList.remove("menuOpen");
-}
-
-var inlogSluit = 
-document.querySelector("header > div > button:nth-of-type(2)");
-
-inlogSluit.addEventListener("click", inlogSluiten);
-
-function inlogSluiten() {
-    deHeader.classList.remove("inlogOpen");
-}
-
-var menuButton = 
-document.querySelector("header > div > button:nth-of-type(3)");
-
-menuButton.addEventListener("click", menuToggle);
+hamburgerMenu.addEventListener("click", menuToggle);
 
 function menuToggle() {
-    deHeader.classList.toggle("menuOpen");
-    deHeader.classList.remove("inlogOpen");
+    hamburgerMenu.classList.toggle("menuOpen");
 }
-
-var derdeButton = 
-document.querySelector("header button:nth-of-type(3)");
-
-function derdeButtonIcon() {
-    derdeButton.classList.toggle("iconchange");
-}
-
-derdeButton.addEventListener("click",derdeButtonIcon);
