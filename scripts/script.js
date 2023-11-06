61835376ce051d9d96124a9d7cd8ee115e2c90cf
Liamvanbart1 <90309539+Liamvanbart1@users.noreply.github.com>
2023-11-07 00:25:27 +0100

    allles veranderd

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,31 @@
 // JavaScript Document
 const deButton = document.querySelector("header nav button:nth-of-type(3)");
 const deMenu = document.querySelector("body>nav");
+const deWinkelwagen = document.querySelector("header nav button:nth-of-type(2)");
+const deWinkelwagenMenu = document.querySelector("body>nav:nth-of-type(2)");
+const deSearchmenu = document.querySelector("header nav button:nth-of-type(1)");
+const deSearch = document.querySelector("body>nav:nth-of-type(3)");
+
 
 deButton.onclick = toggleMenu;
+deWinkelwagen.onclick = toggleWinkelwagen;
+deSearchmenu.onclick = toggleSearch;
 
 function toggleMenu() {
   deButton.classList.toggle("open");
   deMenu.classList.toggle("open");
+  deWinkelwagenMenu.classList.remove("open");
+  deSearch.classList.remove("open");
+}
+
+function toggleWinkelwagen() {
+  deWinkelwagenMenu.classList.toggle("open");
+  deMenu.classList.remove("open");
+  deSearch.classList.remove("open");
+}
+
+function toggleSearch() {
+  deSearch.classList.toggle("open");
+  deMenu.classList.remove("open");
+  deWinkelwagenMenu.classList.remove("open");
 }
