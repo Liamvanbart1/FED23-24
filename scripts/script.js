cc01f2c2093b81b1d2d137f03aa39296b4253d94
Liamvanbart1 <90309539+Liamvanbart1@users.noreply.github.com>
2023-11-07 17:46:58 +0100

    Hele hoop vernaderingen

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,14 @@ const deWinkelwagen = document.querySelector("header nav button:nth-of-type(2)")
 const deWinkelwagenMenu = document.querySelector("body>nav:nth-of-type(2)");
 const deSearchmenu = document.querySelector("header nav button:nth-of-type(1)");
 const deSearch = document.querySelector("body>nav:nth-of-type(3)");
+const deFooter1button = document.querySelector("footer nav button:nth-of-type(1)");
+const deFooter1 = document.querySelector("footer nav ul:nth-of-type(1)");
 
 
 deButton.onclick = toggleMenu;
 deWinkelwagen.onclick = toggleWinkelwagen;
 deSearchmenu.onclick = toggleSearch;
+deFooter1button.onclick = toggleFooter1;
 
 function toggleMenu() {
   deButton.classList.toggle("open");
@@ -22,10 +25,25 @@ function toggleWinkelwagen() {
   deWinkelwagenMenu.classList.toggle("open");
   deMenu.classList.remove("open");
   deSearch.classList.remove("open");
+  deButton.classList.remove("open");
 }
 
 function toggleSearch() {
   deSearch.classList.toggle("open");
   deMenu.classList.remove("open");
   deWinkelwagenMenu.classList.remove("open");
+  deButton.classList.remove("open");
+}
+
+document.addEventListener("keydown", event => {
+  if (event.key == "Escape") {
+    deMenu.classList.remove("open");
+    deWinkelwagenMenu.classList.remove("open");
+    deSearch.classList.remove("open");
+    deButton.classList.remove("open");
+  }
+});
+
+function toggleFooter1() {
+  deFooter1.classList.toggle("open");
 }
